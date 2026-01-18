b91d5d1dea0df67aff4878ef772d152fadb28ce5
GabrielBytza <bytzagabriel031@gmail.com>
2026-01-18 20:54:53 +0200

    Add files via upload

---
+++ discord bot/main.py
@@ -0,0 +1,111 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+secret_role = "Admin"
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Don't dare to swear in this server")
+
+    if "fuck" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Stop swearing")
+
+    if "sex" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"stop the swearing {message.author.mention}, you idiot")
+
+    if "bitch" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} okay dumbfuh, we get it")
+
+    if "nigger" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} mods, ban him")
+
+    if "dick" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Stop it , you are not funny ")
+
+    if "pussy" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}you feel smart when you swear huh? ")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def assign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=secret_role)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} HAS EARNED  HIS ROLEd{secret_role}")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def remove(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=secret_role)
+    if role:
+        await ctx.author.remove_roles(role)
+        await ctx.send(f"{ctx.author.mention} wasn't worth for the {secret_role} role ... ")
+    else:
+        await ctx.send("Role doesn't exist")
+
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"You said {msg}")
+
+@bot.command()
+async def reply(ctx):
+    await ctx.reply("I am not a real persol lol , got at #general-chat💬 so you can talk with real people :)")
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
+@bot.command()
+@commands.has_role(secret_role)
+async def secret(ctx):
+    await ctx.send("Welcome to the club!")
+
+@bot.command()
+async def reply2(ctx):
+    await ctx.send(f"{ctx.author.mention}, here is the Help Guide of the bot : do /poll to make a poll , /reply to reply , /dm to dm ")
+
+@secret.error
+async def secret_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("You do not have permission to do that!")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
